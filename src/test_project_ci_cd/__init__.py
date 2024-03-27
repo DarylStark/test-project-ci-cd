3c78d7175693557ba8a5d78b9febc4db3c316b47
Daryl Stark <daryl@dstark.nl>
2024-03-27 16:14:28 +0100

    Add login function

--- src/test_project_ci_cd/__init__.py
+++ src/test_project_ci_cd/__init__.py
@@ -1,3 +1,8 @@
 """Test package for CI/CD."""
 
 __version__ = "0.1.0"
+
+
+def login(username: str, password: str):
+    """Test login function."""
+    return username == "admin" and password == "admin1"
