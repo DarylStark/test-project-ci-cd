301f38ead2e28b8a12a09a951c7bd436a267e74d
Daryl Stark <daryl@dstark.nl>
2024-03-27 16:21:24 +0100

    Add logout function

--- src/test_project_ci_cd/__init__.py
+++ src/test_project_ci_cd/__init__.py
@@ -2,7 +2,25 @@
 
 __version__ = "0.1.0"
 
+session = None
+
 
 def login(username: str, password: str):
     """Test login function."""
-    return username == "admin" and password == "admin1"
+    global session
+    if username == "admin" and password == "admin1":
+        session = "admin"
+        return True
+    else:
+        return False
+
+
+def logout():
+    """Logout function."""
+    global session
+    try:
+        if not session:
+            raise Exception("No session found")
+        session = None
+    except:
+        print("No session found")
